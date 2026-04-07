c25f4dfbfd47095eaf9de91ad2ecfa1d694b90ad
HybridRCG <hybrid.rcg@gmail.com>
2026-04-07 13:34:17 +0000

    Add: Loading message to nodes page while data populates

--- src/routes/nodes/+page.svelte
+++ src/routes/nodes/+page.svelte
@@ -11,21 +11,16 @@
 	import { getSortedFilteredNodes } from '$lib/common/funcs';
 	import { App } from '$lib/States.svelte';
 	import FilterOnlineBtn from '$lib/parts/FilterOnlineBtn.svelte';
-
 	let showCreate = $state(false);
-
 	let sortMethod = $state('id');
 	let sortDirection = $state<Direction>('up');
 	let filterOnlineStatus = $state<OnlineStatus>('all');
 	let filterString = $state('');
-
 	const Outer = $derived(App.layoutNode.value === 'list' ? CardListPage : CardTilePage);
 	const Inner = $derived(App.layoutNode.value === 'list' ? NodeListCard : NodeTileCard);
-
 	const nodesSortedFiltered = $derived(
 		getSortedFilteredNodes(App.nodes.value, filterString, sortMethod, sortDirection, filterOnlineStatus)
 	)
-
 	function toggle(method: string) {
 		if (method != sortMethod) {
 			sortMethod = method;
@@ -35,32 +30,30 @@
 		}
 	}
 </script>
-
 <Page>
-	<PageHeader title="Nodes" layout={App.layoutNode} bind:show={showCreate} bind:filterString>
-		{#snippet button()}
-			<NodeCreate bind:show={showCreate} />
-		{/snippet}
-	</PageHeader>
-
-	<div
-		class="btn-group px-0 mx-0 py-0 my-0 rounded-md variant-ghost-secondary [&>*+*]:border-primary-500"
-	>
-		<SortBtn bind:value={sortMethod} direction={sortDirection} name="ID" {toggle} />
-		<SortBtn bind:value={sortMethod} direction={sortDirection} name="Name" {toggle} />
-		<SortBtn bind:value={sortMethod} direction={sortDirection} name="Last Seen" {toggle} />
-	</div>
-	<div
-		class="btn-group ml-2 px-0 mx-0 py-0 my-0 rounded-md variant-ghost-secondary [&>*+*]:border-primary-500"
-	>
-		<FilterOnlineBtn bind:value={filterOnlineStatus} status="all" name="All" />
-		<FilterOnlineBtn bind:value={filterOnlineStatus} status="online" name="Online" />
-		<FilterOnlineBtn bind:value={filterOnlineStatus} status="offline" name="Offline" />
-	</div>
-
-	<Outer>
-		{#each nodesSortedFiltered as node}
-			<Inner {node} />
-		{/each}
-	</Outer>
+	{#if App.nodes.value && App.nodes.value.length > 0}
+		<PageHeader title="Nodes" layout={App.layoutNode} bind:show={showCreate} bind:filterString>
+			{#snippet button()}
+				<NodeCreate bind:show={showCreate} />
+			{/snippet}
+		</PageHeader>
+		<div
+			class="btn-group px-0 mx-0 py-0 my-0 rounded-md variant-ghost-secondary [&>*+*]:border-primary-500"
+		>
+			<SortBtn bind:value={sortMethod} direction={sortDirection} name="ID" {toggle} />
+			<SortBtn bind:value={sortMethod} direction={sortDirection} name="Name" {toggle} />
+			<SortBtn bind:value={sortMethod} direction={sortDirection} name="Online" {toggle} />
+			<FilterOnlineBtn bind:value={filterOnlineStatus} />
+		</div>
+		<svelte:component this={Outer}>
+			{#each nodesSortedFiltered as node (node.id)}
+				<svelte:component this={Inner} {node} />
+			{/each}
+		</svelte:component>
+	{:else}
+		<div class="p-8 text-center">
+			<p class="text-xl">Loading nodes...</p>
+			<p class="text-sm text-gray-500">Total nodes in store: {App.nodes.value?.length || 0}</p>
+		</div>
+	{/if}
 </Page>
